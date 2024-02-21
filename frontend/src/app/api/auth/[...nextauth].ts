60a19a94af46c227702e67f53348f7a6c1006977
Kyle Lang <kylel35775@gmail.com>
2024-02-21 13:52:51 -0600

    Set up OAuth server routes, needs setup for Google OAuth.

---
+++ frontend/src/app/api/auth/[...nextauth].ts
@@ -0,0 +1,38 @@
+import type { NextApiRequest, NextApiResponse } from "next"
+import NextAuth from "next-auth"
+import CredentialsProvider from "next-auth/providers/credentials"
+import GoogleProvider from "next-auth/providers/google"
+
+export default async function auth(req: NextApiRequest, res: NextApiResponse) {
+  /*const providers = [
+    CredentialsProvider(...),
+    GoogleProvider(...),
+  ]*/
+  
+  const isDefaultSigninPage = req.method === "GET" && req.query.nextauth.includes("signin")
+
+  // Will hide the `GoogleProvider` when you visit `/api/auth/signin`
+  // if (isDefaultSigninPage) providers.pop()
+
+  if(req.query.nextauth.includes("callback") && req.method === "POST") {
+    console.log(
+      "Handling callback request from my Identity Provider",
+      req.body
+    )
+  }
+
+  // Get a custom cookie value from the request
+  const someCookie = req.cookies["some-custom-cookie"]
+
+  return await NextAuth(req, res, {
+    providers,
+    callbacks: {
+      session({ session, token }) {
+        // Return a cookie value as part of the session
+        // This is read when `req.query.nextauth.includes("session") && req.method === "GET"`
+        session.someCookie = someCookie
+        return session
+      }
+    }
+  })
+}
