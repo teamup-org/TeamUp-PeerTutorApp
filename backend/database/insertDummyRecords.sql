af90ef4a13d09b5f8182b7a2bfa346439552caf9
Brandon Nguyen <the.brandon.nguyen@gmail.com>
2024-02-24 12:41:28 -0600

    Updated database create tables script and database insert dummy records script

--- backend/database/insertDummyRecords.sql
+++ backend/database/insertDummyRecords.sql
@@ -52,14 +52,20 @@ VALUES
     (912345678, 'denise', 'dangerous', 4, 4, 4444444444, 'denise@gmail.com'),
     (112345678, 'edgar', 'excellent', 5, 5, 5555555555, 'edgar@gmail.com');
 
-INSERT INTO tutor
-    (uin, first_name, last_name, major_id, seniority_id, pay_rate, bio_text, picture_url, phone_number, email, average_rating)
+INSERT INTO active_status
+    (status_name)
 VALUES
-    (123456789, 'john', 'smith', 1, 1, 10.00, 'Hi', 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e3/Branden_Loera_Headshot.jpg/394px-Branden_Loera_Headshot.jpg?20231019200804', 1234567890, 'john@gmail.com', 1),
-    (234567891, 'maria', 'garcia', 2, 2, 15.00, 'Hello', 'https://upload.wikimedia.org/wikipedia/commons/0/07/Avantika_Headshot.jpg', 3123456789, 'maria@gmail.com', 2),
-    (345678912, 'james', 'johnson', 3, 3, 20.00, 'Greetings', 'https://upload.wikimedia.org/wikipedia/commons/d/de/Kenneth_Harris_II.jpg', 9012345678, 'james@gmail.com', 3),
-    (456789123, 'david', 'rodriquez', 4, 4, 25.00, 'Salutations', 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/8a/Updated-hs-zameer-rizvi-headshot-1.jpg/510px-Updated-hs-zameer-rizvi-headshot-1.jpg?20231107221652', 8901234567, 'david@gmail.com', 4),
-    (567891234, 'mary', 'martinez', 5, 5, 30.00, 'Howdy', 'https://upload.wikimedia.org/wikipedia/commons/7/77/Melissa_Huang_headshot.jpg', 7890123456, 'mary@gmail.com', 5);
+    ("active"),
+    ("inactive");
+
+INSERT INTO tutor
+    (uin, first_name, last_name, major_id, seniority_id, pay_rate, bio_text, picture_url, phone_number, email, average_rating, active_status)
+VALUES
+    (123456789, 'john', 'smith', 1, 1, 10.00, 'Hi', 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e3/Branden_Loera_Headshot.jpg/394px-Branden_Loera_Headshot.jpg?20231019200804', 1234567890, 'john@gmail.com', 1, 1),
+    (234567891, 'maria', 'garcia', 2, 2, 15.00, 'Hello', 'https://upload.wikimedia.org/wikipedia/commons/0/07/Avantika_Headshot.jpg', 3123456789, 'maria@gmail.com', 2, 2),
+    (345678912, 'james', 'johnson', 3, 3, 20.00, 'Greetings', 'https://upload.wikimedia.org/wikipedia/commons/d/de/Kenneth_Harris_II.jpg', 9012345678, 'james@gmail.com', 3, 1),
+    (456789123, 'david', 'rodriquez', 4, 4, 25.00, 'Salutations', 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/8a/Updated-hs-zameer-rizvi-headshot-1.jpg/510px-Updated-hs-zameer-rizvi-headshot-1.jpg?20231107221652', 8901234567, 'david@gmail.com', 4, 2),
+    (567891234, 'mary', 'martinez', 5, 5, 30.00, 'Howdy', 'https://upload.wikimedia.org/wikipedia/commons/7/77/Melissa_Huang_headshot.jpg', 7890123456, 'mary@gmail.com', 5, 1);
 
 INSERT INTO tutor_location_preference
     (tutor_id, location_id)
@@ -124,14 +130,21 @@ VALUES
     ('single'),
     ('group');
 
-INSERT INTO appointment
-    (appointment_type_id, tutor_id, tutee_id, appointment_date, start_time_id, end_time_id, location_id)
+INSERT INTO appointment_status
+    (status_name)
 VALUES
-    (1, 1, 1, '2024-03-01', 1, 2, 1),
-    (1, 2, 2, '2024-03-02', 1, 3, 2),
-    (1, 3, 3, '2024-03-03', 1, 4, 3),
-    (1, 4, 4, '2024-03-04', 1, 5, 4),
-    (1, 5, 5, '2024-03-05', 2, 3, 5);
+    ('confirmed'),
+    ('pending'),
+    ('cancelled');
+
+INSERT INTO appointment
+    (appointment_type_id, tutor_id, tutee_id, appointment_date, start_time_id, end_time_id, location_id, appointment_status_id)
+VALUES
+    (1, 1, 1, '2024-03-01', 1, 2, 1, 1),
+    (1, 2, 2, '2024-03-02', 1, 3, 2, 2),
+    (1, 3, 3, '2024-03-03', 1, 4, 3, 3),
+    (1, 4, 4, '2024-03-04', 1, 5, 4, 1),
+    (1, 5, 5, '2024-03-05', 2, 3, 5, 2);
 
 INSERT INTO appointment_request
     (request_type_id, appointment_id, initiator_type_id, request_status_id)
@@ -143,13 +156,13 @@ VALUES
     (2, 5, 1, 1);
 
 INSERT INTO tutor_review
-    (appointment_id, rating_id, review_text)
+    (tutor_id, tutee_id, rating_id, review_text)
 VALUES
-    (1, 1, "very bad"),
-    (2, 2, "somewhat bad"),
-    (3, 3, "okay"),
-    (4, 4, "somewhat okay"),
-    (5, 5, "very good");
+    (1, 1, 1, "very bad"),
+    (2, 2, 2, "somewhat bad"),
+    (3, 3, 3, "okay"),
+    (4, 4, 4, "somewhat okay"),
+    (5, 5, 5, "very good");
 
 
 
