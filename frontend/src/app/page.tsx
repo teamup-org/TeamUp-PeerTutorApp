526d75ab8db490ed49fc514c6e24fd0c1a7c4830
wellstrey <wells.t.2024@tamu.edu>
2024-03-06 02:12:17 -0600

    Added button for peer tutor registration

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -17,6 +17,16 @@ const links = [
 
 
 export default function LandingPage() {
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get('email'),
+      password: data.get('password'),
+    });
+  };
+
   return (
     <>
       <header>
