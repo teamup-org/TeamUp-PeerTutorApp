f716c684ea3e20924437fc98a2063cb2ce784d6a
Brandon Nguyen <the.brandon.nguyen@gmail.com>
2024-03-11 14:06:51 -0500

    Brandon: added pagination to tutor review

--- backend/databaseAPI/databaseAPI/databaseAPI/src/main/java/xyz/tamutheo/databaseAPI/tutorReview/TutorReviewService.java
+++ backend/databaseAPI/databaseAPI/databaseAPI/src/main/java/xyz/tamutheo/databaseAPI/tutorReview/TutorReviewService.java
@@ -2,22 +2,27 @@ package xyz.tamutheo.databaseAPI.tutorReview;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import xyz.tamutheo.databaseAPI.util.paginationContainer.PaginationContainerModel;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class TutorReviewService {
     @Autowired
     private TutorReviewMapper tutorReviewMapper;
-    public List<TutorReviewModel> read(Integer appointmentIdEquals,
-                                       Integer numberStarsGreaterThanOrEquals,
-                                       Integer numberStarsLessThanOrEquals,
-                                       String reviewTextContains,
-                                       String tuteeEmailContains,
-                                       String tutorEmailContains,
-                                       Integer limit,
-                                       Integer offset)  {
-        return this.tutorReviewMapper.read(appointmentIdEquals,
+    public PaginationContainerModel read(Integer appointmentIdEquals,
+                                         Integer numberStarsGreaterThanOrEquals,
+                                         Integer numberStarsLessThanOrEquals,
+                                         String reviewTextContains,
+                                         String tuteeEmailContains,
+                                         String tutorEmailContains,
+                                         Integer pageNumber,
+                                         Integer numberEntriesPerPage)  {
+        Integer limit = numberEntriesPerPage != null ? numberEntriesPerPage : null;
+        Integer offset = (numberEntriesPerPage != null) && (pageNumber != null) ? (pageNumber - 1) * numberEntriesPerPage : null;
+        List<TutorReviewModel> tutorReviewModelList = this.tutorReviewMapper.read(appointmentIdEquals,
                 numberStarsGreaterThanOrEquals,
                 numberStarsLessThanOrEquals,
                 reviewTextContains,
@@ -25,6 +30,23 @@ public class TutorReviewService {
                 tutorEmailContains,
                 limit,
                 offset);
+        Integer totalNumberEntries = this.tutorReviewMapper.getTotalNumberEntries(appointmentIdEquals,
+                numberStarsGreaterThanOrEquals,
+                numberStarsLessThanOrEquals,
+                reviewTextContains,
+                tuteeEmailContains,
+                tutorEmailContains);
+        Integer totalNumberPages = numberEntriesPerPage != null ? (int) (Math.ceil((double) totalNumberEntries / numberEntriesPerPage)) : 1;
+        Map<String, Integer> metaDataMap = new HashMap<>();
+        metaDataMap.put("totalNumberEntries", totalNumberEntries);
+        metaDataMap.put("totalNumberPages", totalNumberPages);
+        metaDataMap.put("maximumNumberEntriesPerPage", numberEntriesPerPage);
+        metaDataMap.put("pageNumber", pageNumber);
+        PaginationContainerModel paginationContainerModel = PaginationContainerModel.builder()
+                .data(tutorReviewModelList)
+                .metaData(metaDataMap)
+                .build();
+        return paginationContainerModel;
     }
     public void create(TutorReviewModel tutorReviewModel) {
         this.tutorReviewMapper.create(tutorReviewModel);
