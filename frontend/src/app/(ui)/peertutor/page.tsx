c9d6d63586fdfae17224122e04bf68663fbd7c9c
wellstrey <wells.t.2024@tamu.edu>
2024-03-15 14:08:40 -0500

    Adding Tabs for tutor vs. tutee

--- frontend/src/app/(ui)/peertutor/page.tsx
+++ frontend/src/app/(ui)/peertutor/page.tsx
@@ -5,7 +5,7 @@ import { useState } from 'react';
 
 import { 
   Avatar, Button, CssBaseline, TextField, Link, Grid, Box, Typography, Container, Paper, InputLabel, MenuItem, Select, SelectChangeEvent,
-  OutlinedInput, InputAdornment
+  OutlinedInput, InputAdornment, Tabs, Tab, tabClasses
 } from '@mui/material';
 import AccountBoxIcon from '@mui/icons-material/AccountBox';
 
@@ -14,32 +14,36 @@ import { AnyARecord } from 'dns';
 
 import { useSession, signOut } from 'next-auth/react';
 
-const newTutor = { 
-  active_status_name: "active",
-  //first_name: fields.firstName,
-  first_name: "Trey",
-  //last_name: fields.lastName,
-  last_name: "Wells",
-  listing_title: "csce tutor",
-  //seniority_name: fields.seniority,
-  seniority_name: "senior",
-  //major_abbreviation: fields.major,
-  major_abbreviation: "csce",
-  pay_rate: 40.0,
-  //bio_text: fields.bioText,
-  bio_text: "Howdy everybody!",
-  //picture_url: fields.pfp,
-  picture_url: "fakeline",
-  //phone_number: fields.phoneNumber,
-  phone_number: 2146017139,
-  //email: fields.email,
-  email: "wells.t.2024@tamu.edu"
-  //active_status_name: 1
-};
+interface TabPanelProps {
+  children?: React.ReactNode;
+  dir?: string;
+  index: number;
+  value: number;
+}
 
-export default function SignUp() {
+function TabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
 
-  // Seniority Selection -------------------------------------------------
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`full-width-tabpanel-${index}`}
+      aria-labelledby={`full-width-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          {index === 0 && <PeerTutorForm />}
+          {index === 1 && <TuteeForm />}
+        </Box>
+      )}
+    </div>
+  );
+}
+
+function PeerTutorForm() {
+
   const seniorityOptions = [
     { value: 'freshman', label: 'Freshman' },
     { value: 'sophomore', label: 'Sophomore' },
@@ -58,10 +62,10 @@ export default function SignUp() {
 
   const { data: session, status } = useSession();
 
-  // Submission of the peer tutor form -----------------------------------
+  // Form submission -----------------------------------------------------
 
   const { mutate } = useTutorMutation();
-  
+
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const formData = new FormData(event.currentTarget);
@@ -79,8 +83,260 @@ export default function SignUp() {
       active_status_name: 'active'
     };
     
-
     mutate(registrationData);
+  };
+
+  return (
+    <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={6}>
+          <TextField
+            autoComplete="given-name"
+            name="firstName"
+            required
+            fullWidth
+            id="firstName"
+            label="First Name"
+            autoFocus
+          />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <TextField
+            required
+            fullWidth
+            id="lastName"
+            label="Last Name"
+            name="lastName"
+            autoComplete="family-name"
+          />
+        </Grid>
+
+        <Grid item xs={12}>
+          <TextField
+            required
+            fullWidth
+            id="phoneNumber"
+            label="Phone Number"
+            name="phoneNumber"
+            autoComplete="phoneNumber"
+          />
+        </Grid>
+
+        <Grid item xs={12}>
+          <TextField
+            required
+            fullWidth
+            id="title"
+            label="Title (Ex. Computer Science Tutor)"
+            name="title"
+            autoComplete="title"
+          />
+        </Grid>
+
+        <Grid item xs={12} sm={6}>
+        <InputLabel id="senioritySelect">Seniority</InputLabel>
+        <Select
+          labelId="seniorityLabel"
+          id="seniority"
+          value={seniority}
+          label="seniority"
+          onChange={changeSeniority}
+        >
+          {seniorityOptions.map(option => (
+          <MenuItem key={option.value} value={option.value}>
+            {option.label}
+          </MenuItem>
+        ))}
+        </Select>
+        </Grid>
+
+        <Grid item xs={12} sm={6}>
+          <InputLabel htmlFor="payrate">Pay Rate/Hr</InputLabel>
+          <OutlinedInput
+            name="payrate"
+            id="payrate"
+            startAdornment={<InputAdornment position="start">$</InputAdornment>}
+            label="payrate"
+          />
+        </Grid>
+
+        <Grid item xs={12}>
+          <TextField
+            required
+            fullWidth
+            id="major"
+            label="Undergraduate Department (4 letter abbreviation)"
+            name="major"
+            autoComplete="major"
+          />
+        </Grid>
+
+        <Grid item xs={12}>
+          <TextField
+            required
+            fullWidth
+            id="bioText"
+            label="Biography Text (This can be changed later)"
+            name="bioText"
+            autoComplete="bioText"
+            multiline  // Add this prop for multiline textarea
+            rows={4}   // Optionally set the number of rows
+          />
+        </Grid>
+
+      </Grid>
+      <Button
+        type="submit"
+        fullWidth
+        variant="contained"
+        sx={{ mt: 3, mb: 2 }}
+      >
+        Register
+      </Button>
+    </Box>
+  );
+}
+
+function TuteeForm() {
+
+  const seniorityOptions = [
+    { value: 'freshman', label: 'Freshman' },
+    { value: 'sophomore', label: 'Sophomore' },
+    { value: 'junior', label: 'Junior' },
+    { value: 'senior', label: 'Senior' },
+    { value: 'graduate', label: 'Graduate Student' }
+  ];
+  
+  const [seniority, setSeniority] = React.useState('');
+  
+  const changeSeniority = (event: SelectChangeEvent) => {
+    setSeniority(event.target.value as string);
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    // Perform submission logic for peer tutor form
+  };
+
+  return (
+    <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={6}>
+          <TextField
+            autoComplete="given-name"
+            name="firstName"
+            required
+            fullWidth
+            id="firstName"
+            label="First Name"
+            autoFocus
+          />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <TextField
+            required
+            fullWidth
+            id="lastName"
+            label="Last Name"
+            name="lastName"
+            autoComplete="family-name"
+          />
+        </Grid>
+
+        <Grid item xs={12}>
+          <TextField
+            required
+            fullWidth
+            id="phoneNumber"
+            label="Phone Number"
+            name="phoneNumber"
+            autoComplete="phoneNumber"
+          />
+        </Grid>
+
+        <Grid item xs={12}>
+          <TextField
+            required
+            fullWidth
+            id="title"
+            label="Title (Ex. Computer Science Tutor)"
+            name="title"
+            autoComplete="title"
+          />
+        </Grid>
+
+        <Grid item xs={12} sm={6}>
+        <InputLabel id="senioritySelect">Seniority</InputLabel>
+        <Select
+          labelId="seniorityLabel"
+          id="seniority"
+          value={seniority}
+          label="seniority"
+          onChange={changeSeniority}
+        >
+          {seniorityOptions.map(option => (
+          <MenuItem key={option.value} value={option.value}>
+            {option.label}
+          </MenuItem>
+        ))}
+        </Select>
+        </Grid>
+
+        <Grid item xs={12} sm={6}>
+          <InputLabel htmlFor="payrate">Pay Rate/Hr</InputLabel>
+          <OutlinedInput
+            name="payrate"
+            id="payrate"
+            startAdornment={<InputAdornment position="start">$</InputAdornment>}
+            label="payrate"
+          />
+        </Grid>
+
+        <Grid item xs={12}>
+          <TextField
+            required
+            fullWidth
+            id="major"
+            label="Undergraduate Department (4 letter abbreviation)"
+            name="major"
+            autoComplete="major"
+          />
+        </Grid>
+
+        <Grid item xs={12}>
+          <TextField
+            required
+            fullWidth
+            id="bioText"
+            label="Biography Text (This can be changed later)"
+            name="bioText"
+            autoComplete="bioText"
+            multiline  // Add this prop for multiline textarea
+            rows={4}   // Optionally set the number of rows
+          />
+        </Grid>
+
+      </Grid>
+      <Button
+        type="submit"
+        fullWidth
+        variant="contained"
+        sx={{ mt: 3, mb: 2 }}
+      >
+        Register
+      </Button>
+    </Box>
+  );
+}
+
+export default function Registration() {
+
+  const [tab, setTab] = React.useState(0);
+
+  const tabLabels = ["Register as Peer Tutor", "Register as Tutee"];
+
+  const handleChangeTab = (event: React.SyntheticEvent, newValue: number) => {
+    setTab(newValue);
   };
 
   return (
@@ -96,126 +352,26 @@ export default function SignUp() {
             alignItems: 'center',
           }}
         >
+
+          <Tabs value={tab} onChange={handleChangeTab} centered>
+            <Tab label={tabLabels[0]} />
+            <Tab label={tabLabels[1]} />
+          </Tabs>
+
           <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
             <AccountBoxIcon />
           </Avatar>
           <Typography component="h1" variant="h5">
-            Register as a Peer Tutor
+            {tabLabels[tab]}
           </Typography>
-          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
-            <Grid container spacing={2}>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  autoComplete="given-name"
-                  name="firstName"
-                  required
-                  fullWidth
-                  id="firstName"
-                  label="First Name"
-                  autoFocus
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  required
-                  fullWidth
-                  id="lastName"
-                  label="Last Name"
-                  name="lastName"
-                  autoComplete="family-name"
-                />
-              </Grid>
 
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  id="phoneNumber"
-                  label="Phone Number"
-                  name="phoneNumber"
-                  autoComplete="phoneNumber"
-                />
-              </Grid>
+          <TabPanel value={tab} index={0}>
+            {/* Peer Tutor Form */}
+          </TabPanel>
+          <TabPanel value={tab} index={1}>
+            {/* Tutee Form */}
+          </TabPanel>
 
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  id="title"
-                  label="Title (Ex. Computer Science Tutor)"
-                  name="title"
-                  autoComplete="title"
-                />
-              </Grid>
-
-              <Grid item xs={12} sm={6}>
-              <InputLabel id="senioritySelect">Seniority</InputLabel>
-              <Select
-                labelId="seniorityLabel"
-                id="seniority"
-                value={seniority}
-                label="seniority"
-                onChange={changeSeniority}
-              >
-                {seniorityOptions.map(option => (
-                <MenuItem key={option.value} value={option.value}>
-                  {option.label}
-                </MenuItem>
-              ))}
-              </Select>
-              </Grid>
-
-              <Grid item xs={12} sm={6}>
-                <InputLabel htmlFor="payrate">Pay Rate/Hr</InputLabel>
-                <OutlinedInput
-                  name="payrate"
-                  id="payrate"
-                  startAdornment={<InputAdornment position="start">$</InputAdornment>}
-                  label="payrate"
-                />
-              </Grid>
-
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  id="major"
-                  label="Undergraduate Department (4 letter abbreviation)"
-                  name="major"
-                  autoComplete="major"
-                />
-              </Grid>
-
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  id="bioText"
-                  label="Biography Text (This can be changed later)"
-                  name="bioText"
-                  autoComplete="bioText"
-                  multiline  // Add this prop for multiline textarea
-                  rows={4}   // Optionally set the number of rows
-                />
-              </Grid>
-
-            </Grid>
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Register
-            </Button>
-            <Grid container justifyContent="flex-end">
-              <Grid item>
-                <Link href="/dashboard/profile" variant="body2">
-                  Already Registered? Update Profile
-                </Link>
-              </Grid>
-            </Grid>
-          </Box>
         </Box>
       </Paper>
     </Container>
