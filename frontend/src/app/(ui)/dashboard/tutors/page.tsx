75ae2689150656afa320fa719e5d8956404019d7
Kyle Lang <kylel35775@gmail.com>
2024-03-18 15:19:59 -0500

    Major and course filtration implemented. Working to refactor code.

--- frontend/src/app/(ui)/dashboard/tutors/page.tsx
+++ frontend/src/app/(ui)/dashboard/tutors/page.tsx
@@ -4,7 +4,8 @@ import React, { useState, useEffect, SyntheticEvent }
 from 'react';
 
 import { Box, Container, Grid, Stack, Skeleton, Pagination, Paper, 
-  Slider, Select, SelectChangeEvent, MenuItem, FormControl, InputLabel, Autocomplete, TextField, Typography, ToggleButtonGroup, ToggleButton }
+  Slider, Select, SelectChangeEvent, MenuItem, FormControl, InputLabel, 
+  Autocomplete, TextField, Typography, ToggleButtonGroup, ToggleButton }
 from '@mui/material';
 
 import TutorCard from '@/app/(ui)/tutor-card';
@@ -38,11 +39,12 @@ export default function TutorPage() {
   };
 
   const [page, setPage] = useState(1);
-  const handleChange = (event: React.ChangeEvent<unknown>, value: number) => {
+  const handlePageChange = (event: React.ChangeEvent<unknown>, value: number) => {
     setPage(value);
+    // if (!tutorIsLoading || !tutorIsFetching) window.scrollTo(0, 0);
   };
 
-  const [rateSlider, setRateSlider] = useState([0, 200]);
+  const [rate, setRate] = useState([0, 200]);
   const handleRateChange = (
     event: Event,
     newValue: number | number[],
@@ -53,9 +55,9 @@ export default function TutorPage() {
     }
 
     if (activeThumb === 0) {
-      setRateSlider([Math.min(newValue[0], rateSlider[1] - minKnobDistance), rateSlider[1]]);
+      setRate([Math.min(newValue[0], rate[1] - minKnobDistance), rate[1]]);
     } else {
-      setRateSlider([rateSlider[0], Math.max(newValue[1], rateSlider[0] + minKnobDistance)]);
+      setRate([rate[0], Math.max(newValue[1], rate[0] + minKnobDistance)]);
     }
   };
 
@@ -63,29 +65,42 @@ export default function TutorPage() {
   const handleSortChange = (event: SelectChangeEvent) => {
     let newSortBy: string = event.target.value;
     setSort(newSortBy);
-    window.scrollTo(0, 0);
+    // window.scrollTo(0, 0);
   };
 
-  const [major, setMajor] = useState<{label: string, firstLetter: string} | null>();
+  const [major, setMajor] = useState<{label: string, firstLetter: string} | null>(null);
   const handleMajorChange = (event: any, value: {label: string, firstLetter: string} | null) => {
     setMajor(value);
+  };
+
+  const [course, setCourse] = useState<{label: number, firstNumber: number} | null>(null);
+  const handleCourseChange = (event: any, value: {label: number, firstNumber: number} | null) => {
+    setCourse(value);
   };
 
   // Database Fetching
   const { data: tutorData, isLoading: tutorIsLoading, isFetching: tutorIsFetching, refetch: tutorRefetch } = 
   TableFetch<TutorQuery>("tutor", `number_entries_per_page=${tutorsPerPage}`, `page_number=${page}`, 
     `sort_by=${sort}`,
-    `pay_rate_greater_than_or_equals=${rateSlider[0]}`, 
-    `pay_rate_less_than_or_equals=${rateSlider[1]}`,
-    //`major_abbreviation_contains=${major?.label}`
+    `pay_rate_greater_than_or_equals=${rate[0]}`, 
+    `pay_rate_less_than_or_equals=${rate[1]}`,
+    `course_major_abbreviation_contains=${major ? major.label : ""}`,
+    `course_number_equals=${course ? course.label : ""}`
   );
   
   const { data: majorData, isLoading: majorIsLoading, isFetching: majorIsFetching, isPlaceholderData: majorIsPlaceholderData, refetch: majorRefetch } = 
-  TableFetch<Major[]>("major");
+    TableFetch<Major[]>("major");
   
+  const { data: courseData, isLoading: courseIsLoading, isFetching: courseIsFetching, refetch: courseRefetch } =
+    TableFetch<Course[]>("course", `major_abbreviation_contains=${major?.label}`);
+
   useEffect(() => {
     tutorRefetch();
-  }, [tutorsPerPage, page, sort]);
+  }, [tutorsPerPage, page, sort, major, course]);
+
+  useEffect(() => {
+    courseRefetch();
+  }, [major]);
   
   const printTutors = () => {
     if (tutorIsLoading) {
@@ -98,6 +113,14 @@ export default function TutorPage() {
         <TutorCard tutor={tutor} key={index} />
       ));
     }
+  };
+
+  const populateMajorOptions = () => {
+    if (majorData)
+      return (majorData.map((major: Major) => { 
+        return { label: (major.majorAbbreviation).toUpperCase(), firstLetter: major.majorAbbreviation[0].toUpperCase() }})).sort((a, b) => -b.firstLetter.localeCompare(a.firstLetter))
+    
+    return [];
   };
 
 
@@ -123,7 +146,7 @@ export default function TutorPage() {
                     <Autocomplete 
                       fullWidth loading={majorIsLoading}
                       id="autocomplete-major" 
-                      options={ majorData ? (majorData.map((major: Major) => {return { label: (major.majorAbbreviation).toUpperCase(), firstLetter: major.majorAbbreviation[0].toUpperCase() }})).sort((a, b) => -b.firstLetter.localeCompare(a.firstLetter)) : [] } 
+                      options={ majorData ? (majorData.map((major: Major) => { return { label: (major.majorAbbreviation).toUpperCase(), firstLetter: major.majorAbbreviation[0].toUpperCase() }})).sort((a, b) => -b.firstLetter.localeCompare(a.firstLetter)) : [] } 
                       isOptionEqualToValue={ (option, value) => (option.label === option.label) }
                       groupBy={ (option) => option.firstLetter }
                       value={major || null} onChange={handleMajorChange} 
@@ -136,7 +159,10 @@ export default function TutorPage() {
                     <Autocomplete 
                       fullWidth 
                       id="autocomplete-course" 
-                      options={[]}
+                      options={ courseData ? (courseData.map((course: Course) => { return { label: course.courseNumber, firstNumber: Number(course.courseNumber.toString()[0]) }})).sort((a: any, b: any) => a.label - b.label) : [] }
+                      isOptionEqualToValue={ (option, value) => (option.label === option.label) }
+                      groupBy={ (option) => option.firstNumber.toString() }
+                      value={course || null} onChange={handleCourseChange}
                       renderInput={ (params) => <TextField {...params} label="Course" /> }
                       disabled={ !major }
                     />
@@ -149,7 +175,7 @@ export default function TutorPage() {
                     <Slider 
                       valueLabelDisplay="on" valueLabelFormat={valueLabelFormat} getAriaLabel={() => ""} getAriaValueText={valuetext}
                       min={0} max={200} step={5} 
-                      value={rateSlider} onChange={handleRateChange} onChangeCommitted={() => tutorRefetch()}
+                      value={rate} onChange={handleRateChange} onChangeCommitted={() => tutorRefetch()}
                       disableSwap sx={{ '& .MuiSlider-valueLabel': { top: 4, backgroundColor: 'unset', '& *': { background: 'transparent', color: '#000' } } }}
                     />
                   </Box>
@@ -166,7 +192,7 @@ export default function TutorPage() {
                 <Box display="flex" flexGrow={1} justifyContent="center">
                   <Pagination 
                     color="primary" size="large"
-                    count={tutorData?.metaData?.totalNumberPages} page={page} onChange={handleChange} 
+                    count={tutorData?.metaData?.totalNumberPages} page={page} onChange={handlePageChange} 
                     disabled={tutorIsLoading || tutorIsFetching}
                   />
                 </Box>
