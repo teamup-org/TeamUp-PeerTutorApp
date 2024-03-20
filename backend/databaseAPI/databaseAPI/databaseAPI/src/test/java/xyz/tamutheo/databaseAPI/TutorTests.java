3762ff5fe241eccb42d240007d89d04e3401dd8a
Evan Oliai <ERoliai@sbcglobal.net>
2024-03-20 14:23:29 -0500

    draft in adapting first unit test tutor

---
+++ backend/databaseAPI/databaseAPI/databaseAPI/src/test/java/xyz/tamutheo/databaseAPI/TutorTests.java
@@ -0,0 +1,52 @@
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(TutorController.java)
+public class TutorTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private TutorService tutorService;
+
+    // Test for create endpoint
+    @Test
+    public void testCreateTutor() throws Exception {
+        mockMvc.perform(post("/tutor")
+                .param("active_status_name", "active")
+                .param("bio_text", "Hi! I like money!")
+                .param("email", "eugene.krabs@krusty.krab")
+                .param("first_name", "Eugene")
+                .param("last_name", "Krabs")
+                .param("listing_title", "Founder/Owner of the Krusty Krab")
+                .param("major_abbreviation", "KYKB")
+                .param("pay_rate", "1000000")
+                .param("phone_number", "9999999999")
+                .param("picture_url", "https://upload.wikimedia.org/wikipedia/en/thumb/f/f8/Mr._Krabs.svg/1280px-Mr._Krabs.svg.png")
+                .param("seniority_name", "Senior")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED))
+                .andExpect(status().isOk());
+
+        verify(tutorService).create(any(TutorModel.class));
+    }
+
+
+}
