c9c7acde28a54fa9bbe78fe041c9aa74afc74868
Evan Oliai <ERoliai@sbcglobal.net>
2024-03-24 14:53:42 -0500

    update tutor test

--- backend/databaseAPI/databaseAPI/databaseAPI/src/test/java/xyz/tamutheo/databaseAPI/TutorTests.java
+++ backend/databaseAPI/databaseAPI/databaseAPI/src/test/java/xyz/tamutheo/databaseAPI/TutorTests.java
@@ -52,6 +52,30 @@ public class TutorTests {
         verify(tutorService).create(any(TutorModel.class));
     }
 
+    // Test for update endpoint
+    @Test
+    public void testUpdateTutor() throws Exception {
+        mockMvc.perform(put("/tutor")
+                .param("email_old", "plankton@chum.bucket")
+                .param("active_status_name_new", "active")
+                .param("bio_text_new", "Hi! I like money!")
+                .param("email_new", "eugene.krabs@krusty.krab")
+                .param("first_name_new", "Eugene")
+                .param("last_name_new", "Krabs")
+                .param("listing_title_new", "Founder/Owner of the Krusty Krab")
+                .param("location_preferences_new", "The Krusty Krab")
+                .param("major_abbreviation_new", "KYKB")
+                .param("pay_rate_new", "1000000")
+                .param("phone_number_new", "9999999999")
+                .param("picture_url_new", "https://upload.wikimedia.org/wikipedia/en/thumb/f/f8/Mr._Krabs.svg/1280px-Mr._Krabs.svg.png")
+                .param("seniority_name_new", "Senior")
+                // Add other parameters similarly
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED))
+                .andExpect(status().isOk());
+
+        verify(tutorService).update(any(TutorModel.class), any(TutorModel.class));
+    }
+
     // Test for handling missing required parameters
     @Test
     public void testCreateTutorMissingParameters() throws Exception {
