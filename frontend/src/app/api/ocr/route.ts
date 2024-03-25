e067ed92f636fa1118d2ed2b73556c06252ab760
wellstrey <wells.t.2024@tamu.edu>
2024-03-24 19:46:49 -0500

    added documentai stuff

--- frontend/src/app/api/ocr/route.ts
+++ frontend/src/app/api/ocr/route.ts
@@ -1,12 +1,72 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
+const projectId = 'csce-482-capstone';
+const location = 'us'; 
+const processorId = '4acd20a8f0372a93';
+
+const {DocumentProcessorServiceClient} =
+  require('@google-cloud/documentai').v1;
+
+// Instantiates a client
+// apiEndpoint regions available: eu-documentai.googleapis.com, us-documentai.googleapis.com (Required if using eu based processor)
+// const client = new DocumentProcessorServiceClient({apiEndpoint: 'eu-documentai.googleapis.com'});
+const client = new DocumentProcessorServiceClient();
+
+async function quickstart(transcript: string) {
+  // The full resource name of the processor, e.g.:
+  // projects/project-id/locations/location/processor/processor-id
+  // You must create new processors in the Cloud Console first
+  const name = `projects/${projectId}/locations/${location}/processors/${processorId}`;
+
+  // Convert the image data to a Buffer and base64 encode it.
+  const encodedImage = transcript;
+
+  const request = {
+    name,
+    rawDocument: {
+      content: encodedImage,
+      mimeType: 'application/pdf',
+    },
+  };
+
+  // Recognizes text entities in the PDF document
+  const [result] = await client.processDocument(request);
+  const {document} = result;
+
+  // Get all of the document text as one big string
+  const {text} = document;
+
+  // Extract shards from the text field
+  const getText = (textAnchor: any) => {
+    if (!textAnchor.textSegments || textAnchor.textSegments.length === 0) {
+      return '';
+    }
+
+    // First shard in document doesn't have startIndex property
+    const startIndex = textAnchor.textSegments[0].startIndex || 0;
+    const endIndex = textAnchor.textSegments[0].endIndex;
+
+    return text.substring(startIndex, endIndex);
+  };
+
+  // Read the text recognition output from the processor
+  console.log('The document contains the following paragraphs:');
+  const [page1] = document.pages;
+  const {paragraphs} = page1;
+
+  for (const paragraph of paragraphs) {
+    const paragraphText = getText(paragraph.layout.textAnchor);
+    console.log(`Paragraph text:\n${paragraphText}`);
+  }
+}
+
 export async function GET (request: Request) {
     return Response.json({ text: 'This is the GET request' });
 }
 
-export async function POST(req: any, res: any){
+export async function POST(req: Request, res: Response){
     const data = await req.json()
-    console.log(data)
-
-    return Response.json(data)
+    const { transcript } = data;
+    await quickstart(transcript);
+    return Response.json({ status: 200})
 }
