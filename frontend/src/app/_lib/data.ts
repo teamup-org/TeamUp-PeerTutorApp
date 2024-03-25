d9e320736fee8fd7dce14e6d2961c1defd87c2f3
wellstrey <wells.t.2024@tamu.edu>
2024-03-25 11:34:54 -0500

    changed database-api to database_api

--- frontend/src/app/_lib/data.ts
+++ frontend/src/app/_lib/data.ts
@@ -9,7 +9,7 @@ import axios from "axios";
 
 // Base URL's to direct backend database queries to
 const development = "http://localhost:8080";            // For use in local development, forwards to a local instance on port 8080, in this case forwarding to Java Sprint Boot
-const deployment = "https://tamutheo.xyz/database-api"; // For use on deployment server, forwards to Spring Boot endpoint 'database-api'
+const deployment = "https://tamutheo.xyz/database_api"; // For use on deployment server, forwards to Spring Boot endpoint 'database-api'
 axios.defaults.baseURL = development;
 
 
