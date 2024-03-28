72dffb9066224df4eac7c2b91a25b31c285dc389
Kyle Lang <kylel35775@gmail.com>
2024-03-28 02:43:26 -0500

    Refactored some code into separate component files, such as tutor-profile, review. Pop-up box for tutors is still WIP

--- frontend/src/app/(ui)/tutor-card.tsx
+++ frontend/src/app/(ui)/tutor-card.tsx
@@ -1,18 +1,21 @@
 'use client';
 
+
 // import { useRef } from 'react';
 
 import Image from 'next/image';
 
-import { Box, Stack, Card, Typography, Divider, Avatar, Rating, Chip, Accordion, AccordionSummary, AccordionDetails } 
+import type { SkeletonProps } from '@mui/material';
+import { Box, Stack, Card, Typography, Divider, Avatar, Rating, Chip, Skeleton } 
 from '@mui/material';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 import { toTitleCase }
 from '@/app/_lib/utils'
+import React from 'react';
 
-type TutorCardProps = { tutor: Tutor };
-export default function TutorCard(props: TutorCardProps) {
+type TutorCardProps = { tutor: Tutor, elevation: number };
+export function TutorCard(props: TutorCardProps) {
   /*const stackRef = useRef<HTMLDivElement>(null);
   const scrollRight = () => {
     if(stackRef.current) stackRef.current.scrollLeft += 100;
@@ -25,16 +28,15 @@ export default function TutorCard(props: TutorCardProps) {
   };
 
   return (
-    <Card elevation={4} sx={{ p: 2, width: '100%' }}>
+    <Card elevation={props.elevation} sx={{ p: 2, width: '100%' }}>
       <Stack direction="row">
         <Stack direction="column" alignItems="center" spacing={1} minWidth="0%" width="20%">
-          <Typography variant="h6"> <center> {toTitleCase(props.tutor?.firstName + " " + props.tutor?.lastName)} </center> </Typography>
+          <Typography variant="h6" textAlign="center"> {toTitleCase(props.tutor?.firstName + " " + props.tutor?.lastName)} </Typography>
 
           <Avatar src={props.tutor?.pictureUrl} alt="Tutor profile picture" sx={{ width: 100, height: 100 }} />
 
-          <Typography variant="h6" width="100%" sx={{ fontWeight: 'bold' }}>
-            <center> ${props.tutor?.payRate} / hr </center>
-            <Divider variant="middle" orientation="horizontal" flexItem/>
+          <Typography variant="h6" textAlign="center" borderBottom={1} borderColor="divider" sx={{ fontWeight: 'bold' }}>
+            ${props.tutor?.payRate} / hr
           </Typography>
 
           <Stack direction="column" alignItems="center">
@@ -72,15 +74,106 @@ export default function TutorCard(props: TutorCardProps) {
           <Typography align="left">
             {props.tutor?.bioText}
           </Typography>
-
-          {/*<Accordion>
-            <AccordionSummary expandIcon={ <ExpandMoreIcon /> } aria-controls="tutor-content" id="tutor-header" />
-            <AccordionDetails>
-              Test
-            </AccordionDetails>
-            </Accordion>*/}
         </Stack>
       </Stack>
+    </Card>
+  );
+}
+
+
+export function TutorSkeleton() {
+  const SkeletonWave = (props: SkeletonProps) => {
+    return (
+      <Skeleton animation="wave" {...props}>
+        {props.children}
+      </Skeleton>
+    );
+  };
+
+
+  return (
+    <Card elevation={4} sx={{ p: 2, width: '100%' }}>
+    <Stack direction="row">
+      <Stack direction="column" alignItems="center" spacing={1} minWidth="0%" width="20%">
+        <SkeletonWave>
+          <Typography variant="h6" textAlign="center"> AAAAAAAAA </Typography>
+        </SkeletonWave>
+
+        <SkeletonWave variant="circular">
+          <Avatar sx={{ width: 100, height: 100 }} />
+        </SkeletonWave>
+
+        <SkeletonWave>
+          <Typography variant="h6" width="100%" textAlign="center" sx={{ fontWeight: 'bold' }}>
+            $50 / hr
+          </Typography>
+        </SkeletonWave>
+
+        <Divider variant="middle" orientation="horizontal" flexItem/>
+
+        <Stack direction="column" alignItems="center">
+          <SkeletonWave>
+            <Rating name="read-only" readOnly/>
+          </SkeletonWave>
+
+          <SkeletonWave>
+            <Typography variant="body1" sx={{ fontSize: 10, fontWeight: 'bold' }}> (AAA) </Typography>
+          </SkeletonWave>
+        </Stack>
+      </Stack>
+
+      <Divider variant="middle" orientation="vertical" flexItem sx={{ mx: 2 }}/>
+
+      <Stack direction="column" spacing={1} minWidth="0%" width="80%">
+        <SkeletonWave>
+          <Typography variant="h6" align="left"> AAAAAAAAAAAAAAAAAAAAAA </Typography>
+        </SkeletonWave>
+
+        <Stack direction="row" spacing={1}>
+          <Stack 
+            direction="row" 
+            spacing={1} 
+            whiteSpace="nowrap" 
+          >
+            <SkeletonWave variant="rounded">
+              <Chip label="AAAAAAAA" /> 
+            </SkeletonWave>
+            <SkeletonWave variant="rounded">
+              <Chip label="AAAAAAAA" /> 
+            </SkeletonWave>
+            <SkeletonWave variant="rounded">
+              <Chip label="AAAAAAAA" /> 
+            </SkeletonWave>
+            <SkeletonWave variant="rounded">
+              <Chip label="AAAAAAAA" /> 
+            </SkeletonWave>
+            <SkeletonWave variant="rounded">
+              <Chip label="AAAAAAAA" /> 
+            </SkeletonWave>
+          </Stack>
+        </Stack>
+
+        <Stack direction="column" spacing={2} pt={4}>
+          <SkeletonWave>
+            <Typography align="left">
+              AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+            </Typography>
+          </SkeletonWave>
+
+          <SkeletonWave>
+            <Typography align="left">
+              AAAAAAAAAAAAAAAAAAAAAAAAAAAA
+            </Typography>
+          </SkeletonWave>
+
+          <SkeletonWave>
+            <Typography align="left">
+              AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+            </Typography>
+          </SkeletonWave>
+        </Stack>
+      </Stack>
+    </Stack>
     </Card>
   );
 }
