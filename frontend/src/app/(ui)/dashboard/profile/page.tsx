755aed62b6784458174e8b0051ca618449880e58
wellstrey <wells.t.2024@tamu.edu>
2024-03-31 18:57:38 -0500

    Switch to ocr

--- frontend/src/app/(ui)/dashboard/profile/page.tsx
+++ frontend/src/app/(ui)/dashboard/profile/page.tsx
@@ -1,10 +1,11 @@
 'use client'
 import React, { useState, ChangeEvent, useEffect } from 'react';
-import { Card, CardContent, Skeleton, CardMedia, TextField, Grid, Button, Typography, Divider, Avatar, Paper, Link } from '@mui/material';
+import { Card, CardContent, Skeleton, CardMedia, TextField, Grid, Button, Typography, Divider, Avatar, Paper, Link, Tab, Tabs } from '@mui/material';
 import { TableFetch, TableUpdate } from '@/app/_lib/data';
 
 import { useSession } from 'next-auth/react';
 import { ConnectingAirportsOutlined } from '@mui/icons-material';
+const tabLabels = ["Peer Tutor Profile Information", "Tutee Profile Information"];
 
 const mutateData = (email: string) => {
   TableUpdate<void>('tutor', [], `email_old=${email}`, `first_name_new=Corbin`);
@@ -59,8 +60,9 @@ function EditableProfileField({ label, value, onSave }: EditableProfileFieldProp
 function ProfilePage() {
   const { data: session, status } = useSession();
   const email = session?.user?.email;
+  const [tab, setTab] = React.useState(0);
 
-  const [profileData, setProfileData] = useState<Tutor>({
+  const [tutorProfileData, setTutorProfileData] = useState<Tutor>({
     activeStatusName: "active",
     firstName: "",
     lastName: "",
@@ -81,17 +83,25 @@ function ProfilePage() {
   const { data: tutorData, isLoading: tutorIsLoading, isFetching: tutorIsFetching, refetch: tutorRefetch } = 
     TableFetch<TutorQuery>("tutor", [], `email_contains=${email}`);
     
-  TableUpdate<void>('tutor', [], `email_old=${email}`, `first_name_new=Corbin`);
+  const { data: tuteeData, isLoading: tuteeIsLoading, isFetching: tuteeIsFetching, refetch: tuteeRefetch } = 
+    TableFetch<TuteeQuery>("tutee", [], `email_contains=${email}`);
+    
+  //TableUpdate<void>('tutor', [], `email_old=${email}`, `first_name_new=Corbin`);
 
-    useEffect(() => {
-      tutorRefetch();
-    }, [email]);
+  useEffect(() => {
+    tutorRefetch();
+    tuteeRefetch();
+  }, [session]);
+
+  const handleChangeTab = (event: React.SyntheticEvent, newValue: number) => {
+    setTab(newValue);
+  };
 
 
   useEffect(() => {
     if (tutorData?.data && (tutorData.data.length > 0)) {
       const tutor = tutorData.data[0];
-      setProfileData({
+      setTutorProfileData({
         activeStatusName: tutor.activeStatusName,
         firstName: tutor.firstName,
         lastName: tutor.lastName,
@@ -111,8 +121,8 @@ function ProfilePage() {
     }
   }, [tutorData]);
 
-  const handleInputChange = (field: keyof typeof profileData) => (newValue: string | number) => {
-    setProfileData({ ...profileData, [field]: newValue });
+  const handleInputChange = (field: keyof typeof tutorProfileData) => (newValue: string | number) => {
+    setTutorProfileData({ ...tutorProfileData, [field]: newValue });
   };
 
   return (
@@ -126,10 +136,18 @@ function ProfilePage() {
       {(tutorIsLoading || tutorIsFetching) && (
         <Skeleton animation="wave" variant="rounded" width="100%"> </Skeleton>
       )}
-      {(tutorData?.data?.length && (!tutorIsLoading && !tutorIsFetching)) ? (
+
+        <Tabs value={tab} onChange={handleChangeTab} centered>
+          <Tab label={tabLabels[0]} />
+          <Tab label={tabLabels[1]} />
+        </Tabs>
+
+      {(tab === 0 && tutorData?.data?.length && (!tutorIsLoading && !tutorIsFetching)) && (
       <div>
+
       <Card style={{ width: '80%', margin: 'auto', marginTop: 50, display: 'flex' }}>
         <CardContent style={{ padding: '20px' }}>
+
           <Typography variant="h6" gutterBottom>
             General Info
           </Typography>
@@ -138,49 +156,49 @@ function ProfilePage() {
             <Grid item xs={6}>
               <EditableProfileField
                 label="First Name"
-                value={profileData.firstName}
+                value={tutorProfileData.firstName}
                 onSave={handleInputChange('firstName')}
               />
             </Grid>
             <Grid item xs={6}>
               <EditableProfileField
                 label="Last Name"
-                value={profileData.lastName}
+                value={tutorProfileData.lastName}
                 onSave={handleInputChange('lastName')}
               />
             </Grid>
             <Grid item xs={12}>
               <EditableProfileField
                 label="Phone Number"
-                value={profileData.phoneNumber}
+                value={tutorProfileData.phoneNumber}
                 onSave={handleInputChange('phoneNumber')}
               />
             </Grid>
             <Grid item xs={12}>
               <EditableProfileField
                 label="Listing Title"
-                value={profileData.listingTitle}
+                value={tutorProfileData.listingTitle}
                 onSave={handleInputChange('listingTitle')}
               />
             </Grid>
             <Grid item xs={6}>
               <EditableProfileField
                 label="Major Abbreviation"
-                value={profileData.majorAbbreviation}
+                value={tutorProfileData.majorAbbreviation}
                 onSave={handleInputChange('majorAbbreviation')}
               />
             </Grid>
             <Grid item xs={6}>
               <EditableProfileField
                 label="Pay Rate"
-                value={profileData.payRate}
+                value={tutorProfileData.payRate}
                 onSave={handleInputChange('payRate')}
               />
             </Grid>
             <Grid item xs={12}>
               <EditableProfileField
                 label="Bio Text"
-                value={profileData.bioText}
+                value={tutorProfileData.bioText}
                 onSave={handleInputChange('bioText')}
               />
             </Grid>
@@ -190,6 +208,7 @@ function ProfilePage() {
           </Button>
         </CardContent>
       </Card>
+        
 
       <Card style={{ width: '80%', margin: 'auto', marginTop: 50, display: 'flex' }}>
         <CardContent style={{ padding: '20px' }}>
@@ -200,7 +219,8 @@ function ProfilePage() {
         </CardContent>
       </Card>
       </div>
-      ) : (
+      )}
+      {(tab === 0 && !tutorData?.data?.length && (!tutorIsLoading && !tutorIsFetching)) && (
         <div style={{ display: 'flex', justifyContent: 'center' }}>
         <Paper style={{ padding: '20px', textAlign: 'center' }}>
           <Typography variant="h4" gutterBottom>
@@ -213,8 +233,102 @@ function ProfilePage() {
           </Typography>
         </Paper>
       </div>
-      )
-    }
+      )}
+      {(tab === 1 && tuteeData?.length && (!tuteeIsLoading && !tuteeIsFetching)) && (
+      <div>
+
+      <Card style={{ width: '80%', margin: 'auto', marginTop: 50, display: 'flex' }}>
+        <CardContent style={{ padding: '20px' }}>
+
+          <Typography variant="h6" gutterBottom>
+            General Info
+          </Typography>
+          <Divider style={{ marginBottom: '20px' }} />
+          <Grid container spacing={2}>
+            <Grid item xs={6}>
+              <EditableProfileField
+                label="First Name"
+                value={tutorProfileData.firstName}
+                onSave={handleInputChange('firstName')}
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <EditableProfileField
+                label="Last Name"
+                value={tutorProfileData.lastName}
+                onSave={handleInputChange('lastName')}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <EditableProfileField
+                label="Phone Number"
+                value={tutorProfileData.phoneNumber}
+                onSave={handleInputChange('phoneNumber')}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <EditableProfileField
+                label="Listing Title"
+                value={tutorProfileData.listingTitle}
+                onSave={handleInputChange('listingTitle')}
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <EditableProfileField
+                label="Major Abbreviation"
+                value={tutorProfileData.majorAbbreviation}
+                onSave={handleInputChange('majorAbbreviation')}
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <EditableProfileField
+                label="Pay Rate"
+                value={tutorProfileData.payRate}
+                onSave={handleInputChange('payRate')}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <EditableProfileField
+                label="Bio Text"
+                value={tutorProfileData.bioText}
+                onSave={handleInputChange('bioText')}
+              />
+            </Grid>
+          </Grid>
+          <Button variant="contained" color="primary" onClick={() => email && mutateData(email)}>
+            Click me!
+          </Button>
+
+        </CardContent>
+      </Card>
+
+      <Card style={{ width: '80%', margin: 'auto', marginTop: 50, display: 'flex' }}>
+        <CardContent style={{ padding: '20px' }}>
+          <Typography variant="h6" gutterBottom>
+            Eligible Courses
+          </Typography>
+          <Divider style={{ marginBottom: '20px' }} />
+        </CardContent>
+      </Card>
+      </div>
+      
+      )}
+      {(tab === 1 && !tuteeData?.length && (!tuteeIsLoading && !tuteeIsFetching)) && (
+        <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <Paper style={{ padding: '20px', textAlign: 'center' }}>
+          <Typography variant="h4" gutterBottom>
+            We cannot find a Tutee Account with this Email
+          </Typography>
+          <Typography variant="h4" gutterBottom>
+            <Button variant="contained" color="primary" href='/register'>
+              Click Here to Register!!
+            </Button>
+          </Typography>
+        </Paper>
+      </div>
+      )}
+
+        
     </div>
   );
 }
