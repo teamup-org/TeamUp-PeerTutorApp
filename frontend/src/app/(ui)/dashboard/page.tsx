df846b4cb7991c15554a913496b5ee5c8f466e57
Kyle Lang <kylel35775@gmail.com>
2024-04-01 14:10:37 -0500

    Added basic pending review to dashboard.

--- frontend/src/app/(ui)/dashboard/page.tsx
+++ frontend/src/app/(ui)/dashboard/page.tsx
@@ -1,16 +1,29 @@
 
-import { Box, Container, Paper, Typography } 
+import { Box, Container, Grid, Stack, Paper, Typography } 
 from '@mui/material';
 
 export default function DashboardPage() {
   return (
     <Box position="relative" sx={{top: 50}}>
-      <Container maxWidth="sm">
-        <Paper elevation={4} sx={{mt:5}}>
-          <Typography variant="h2" align="center">
-            Dashboard
-          </Typography>
-        </Paper>
+      <Container maxWidth="lg">
+        
+        <Grid container>
+          <Grid item md={8}>
+
+          </Grid>
+
+          <Grid item md={4}>
+
+          <Paper elevation={4} sx={{ p: 2 }}>
+            <Typography align="center" variant="h4" borderBottom={1} borderColor="divider"> Pending Reviews </Typography>
+
+            <Stack direction="column">
+
+            </Stack>
+          </Paper>
+          </Grid>
+        </Grid>
+
       </Container>
     </Box>
   );
