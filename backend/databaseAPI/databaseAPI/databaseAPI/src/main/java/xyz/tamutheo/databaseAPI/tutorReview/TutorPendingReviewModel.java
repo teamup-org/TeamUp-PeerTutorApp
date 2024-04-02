4c63eefb437bcd78d809388538e49814f3213563
Brandon Nguyen <the.brandon.nguyen@gmail.com>
2024-04-02 11:23:22 -0500

    Brandon: removed extraneous fields from tutor pending reviews

--- backend/databaseAPI/databaseAPI/databaseAPI/src/main/java/xyz/tamutheo/databaseAPI/tutorReview/TutorPendingReviewModel.java
+++ backend/databaseAPI/databaseAPI/databaseAPI/src/main/java/xyz/tamutheo/databaseAPI/tutorReview/TutorPendingReviewModel.java
@@ -15,20 +15,28 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 public class TutorPendingReviewModel {
     Integer appointmentId;
+    @JsonIgnore
     String appointmentSizeName;
+    @JsonIgnore
     String cancellationReason;
     String endDateTimeString;
     @JsonIgnore
     LocalDateTime endDateTimeValue;
+    @JsonIgnore
     Boolean isCancelled;
+    @JsonIgnore
     Boolean isConfirmed;
     String locationName;
     String startDateTimeString;
     @JsonIgnore
     LocalDateTime startDateTimeValue;
+    @JsonIgnore
     String tuteeEmail;
+    @JsonIgnore
     String tuteeFirstName;
+    @JsonIgnore
     String tuteeLastName;
+    @JsonIgnore
     Long tuteePhoneNumber;
     String tutorEmail;
     String tutorFirstName;
