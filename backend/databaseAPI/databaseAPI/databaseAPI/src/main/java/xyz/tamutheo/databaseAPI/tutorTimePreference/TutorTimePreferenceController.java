22a099f53f22df3f5403251f98dbbd815bf343ec
Brandon Nguyen <the.brandon.nguyen@gmail.com>
2024-04-02 19:48:49 -0500

    Brandon: changed tutor time preference endpoint to accomodate batch requests

--- backend/databaseAPI/databaseAPI/databaseAPI/src/main/java/xyz/tamutheo/databaseAPI/tutorTimePreference/TutorTimePreferenceController.java
+++ backend/databaseAPI/databaseAPI/databaseAPI/src/main/java/xyz/tamutheo/databaseAPI/tutorTimePreference/TutorTimePreferenceController.java
@@ -14,25 +14,8 @@ public class TutorTimePreferenceController {
     private TutorTimePreferenceService tutorTimePreferenceService;
 
     @GetMapping(value = {"", "/"})
-    public List<TutorTimePreferenceModel> read(@RequestParam(name = "end_time_less_than_or_equals", required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.TIME) LocalTime endTimeLessThanOrEquals,
-                                               @RequestParam(name = "start_time_greater_than_or_equals", required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.TIME) LocalTime startTimeGreaterThanOrEquals,
-                                               @RequestParam(name = "tutor_email_equals", required = false) String tutorEmailEquals,
-                                               @RequestParam(name = "weekday_name_in", required = false) String weekdayNameIn,
-                                               @RequestParam(name = "limit", required = false) Integer limit,
-                                               @RequestParam(name = "offset", required = false) Integer offset) {
-        List<String> weekdayNameInList = null;
-        if (weekdayNameIn != null) {
-            weekdayNameInList = Arrays.asList(weekdayNameIn.split(", "));
-            for (int idx = 0; idx < weekdayNameInList.size(); idx++) {
-                weekdayNameInList.set(idx, weekdayNameInList.get(idx).trim());
-            }
-        }
-        return this.tutorTimePreferenceService.read(endTimeLessThanOrEquals,
-                startTimeGreaterThanOrEquals,
-                tutorEmailEquals,
-                weekdayNameInList,
-                limit,
-                offset);
+    public List<TutorTimePreferenceModel> read(@RequestParam(name = "tutor_email_equals") String tutorEmailEquals) {
+        return this.tutorTimePreferenceService.read(tutorEmailEquals);
 
     }
 
@@ -41,47 +24,70 @@ public class TutorTimePreferenceController {
     // expects time input as a comma separated String
     // template: <start_time> <end_time>
     // example: 05:00:00 06:00:00, 09:00:00 11:00:00, 15:00:00 16:00:00
-    public void update(@RequestParam(name = "time_intervals", defaultValue = "empty") String timeIntervals,
-                       @RequestParam(name = "tutor_email") String tutorEmail,
-                       @RequestParam(name = "weekday_name") String weekdayName) {
+    public void update(@RequestParam(name = "monday_time_intervals", defaultValue = "empty") String mondayTimeIntervals,
+                       @RequestParam(name = "tuesday_time_intervals", defaultValue = "empty") String tuesdayTimeIntervals,
+                       @RequestParam(name = "wednesday_time_intervals", defaultValue = "empty") String wednesdayTimeIntervals,
+                       @RequestParam(name = "thursday_time_intervals", defaultValue = "empty") String thursdayTimeIntervals,
+                       @RequestParam(name = "friday_time_intervals", defaultValue = "empty") String fridayTimeIntervals,
+                       @RequestParam(name = "saturday_time_intervals", defaultValue = "empty") String saturdayTimeIntervals,
+                       @RequestParam(name = "sunday_time_intervals", defaultValue = "empty") String sundayTimeIntervals,
+                       @RequestParam(name = "tutor_email") String tutorEmail) {
 
-        List<TutorTimePreferenceModel> tutorTimePreferenceModelList = null;
-        if ((timeIntervals != null) && !(timeIntervals.equals("empty"))) {
-            // parse time intervals into time pairs
-            List<List<LocalTime>> timePairs = new ArrayList<>();
-            String[] timeIntervalList = timeIntervals.split(", ");
-            for (String timeInterval : timeIntervalList) {
-                List<String> timeInfo = Arrays.asList(timeInterval.trim().split(" "));
-                timePairs.add(new ArrayList<>(Arrays.asList(LocalTime.parse(timeInfo.get(0)), LocalTime.parse(timeInfo.get(1)))));
-            }
-            // sort time pairs
-            Collections.sort(timePairs, (a, b) -> a.get(0) == b.get(0) ? a.get(1).toSecondOfDay() - b.get(1).toSecondOfDay() : a.get(0).toSecondOfDay() - b.get(0).toSecondOfDay());
-            // merge overlapping time pairs
-            LinkedList<List<LocalTime>> merged = new LinkedList<>();
-            for (List<LocalTime> timePair : timePairs) {
-                // if list of merged intervals is empty or if the current interval does not overlap with previous, append it
-                if (merged.isEmpty() || merged.getLast().get(1).isBefore(timePair.get(0))) {
-                    merged.add(timePair);
+        HashMap<String, String> hashMap = new HashMap<>();
+        hashMap.put("monday", mondayTimeIntervals);
+        hashMap.put("tuesday", tuesdayTimeIntervals);
+        hashMap.put("wednesday", wednesdayTimeIntervals);
+        hashMap.put("thursday", thursdayTimeIntervals);
+        hashMap.put("friday", fridayTimeIntervals);
+        hashMap.put("saturday", saturdayTimeIntervals);
+        hashMap.put("sunday", sundayTimeIntervals);
+
+        for (String weekdayName : hashMap.keySet()) {
+            System.out.println(weekdayName);
+            List<TutorTimePreferenceModel> tutorTimePreferenceModelList = null;
+            if ((hashMap.get(weekdayName) != null) && !(hashMap.get(weekdayName).equals("empty"))) {
+                // parse time intervals into time pairs
+                List<List<LocalTime>> timePairs = new ArrayList<>();
+                String[] timeIntervalList = hashMap.get(weekdayName).split(", ");
+                for (String timeInterval : timeIntervalList) {
+                    List<String> timeInfo = Arrays.asList(timeInterval.trim().split(" "));
+                    LocalTime startTime = LocalTime.parse(timeInfo.get(0));
+                    LocalTime endTime = LocalTime.parse(timeInfo.get(1));
+                    // skip invalid pairs
+                    if (startTime.isAfter(endTime)) {
+                        continue;
+                    }
+                    timePairs.add(new ArrayList<>(Arrays.asList(startTime, endTime)));
                 }
-                // otherwise, there is an overlap, so merge the current and previous intervals
-                else {
-                    if (timePair.get(1).isAfter(merged.getLast().get(1))) {
-                        merged.getLast().set(1, timePair.get(1));
+                // sort time pairs
+                Collections.sort(timePairs, (a, b) -> a.get(0) == b.get(0) ? a.get(1).toSecondOfDay() - b.get(1).toSecondOfDay() : a.get(0).toSecondOfDay() - b.get(0).toSecondOfDay());
+                // merge overlapping time pairs
+                LinkedList<List<LocalTime>> merged = new LinkedList<>();
+                for (List<LocalTime> timePair : timePairs) {
+                    // if list of merged intervals is empty or if the current interval does not overlap with previous, append it
+                    if (merged.isEmpty() || merged.getLast().get(1).isBefore(timePair.get(0))) {
+                        merged.add(timePair);
+                    }
+                    // otherwise, there is an overlap, so merge the current and previous intervals
+                    else {
+                        if (timePair.get(1).isAfter(merged.getLast().get(1))) {
+                            merged.getLast().set(1, timePair.get(1));
+                        }
                     }
                 }
+                // create list of model objects
+                tutorTimePreferenceModelList = new ArrayList<>();
+                for (List<LocalTime> mergedPair : merged) {
+                    TutorTimePreferenceModel tutorTimePreferenceModel = TutorTimePreferenceModel.builder()
+                            .startTimeValue(mergedPair.get(0))
+                            .endTimeValue(mergedPair.get(1))
+                            .tutorEmail(tutorEmail)
+                            .weekdayName(weekdayName)
+                            .build();
+                    tutorTimePreferenceModelList.add(tutorTimePreferenceModel);
+                }
             }
-            // create list of model objects
-            tutorTimePreferenceModelList = new ArrayList<>();
-            for (List<LocalTime> mergedPair : merged) {
-                TutorTimePreferenceModel tutorTimePreferenceModel = TutorTimePreferenceModel.builder()
-                        .startTimeValue(mergedPair.get(0))
-                        .endTimeValue(mergedPair.get(1))
-                        .tutorEmail(tutorEmail)
-                        .weekdayName(weekdayName)
-                        .build();
-                tutorTimePreferenceModelList.add(tutorTimePreferenceModel);
-            }
+            this.tutorTimePreferenceService.update(tutorTimePreferenceModelList, tutorEmail, weekdayName);
         }
-        this.tutorTimePreferenceService.update(tutorTimePreferenceModelList, tutorEmail, weekdayName);
     }
 }
