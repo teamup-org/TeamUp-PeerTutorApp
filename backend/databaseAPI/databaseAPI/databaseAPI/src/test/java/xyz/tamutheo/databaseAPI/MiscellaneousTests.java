776bb85c2790fcba206e0711fd0cec4f339afeff
Evan Oliai <ERoliai@sbcglobal.net>
2024-04-06 22:11:53 -0500

    Additional unit tests (course)

--- backend/databaseAPI/databaseAPI/databaseAPI/src/test/java/xyz/tamutheo/databaseAPI/MiscellaneousTests.java
+++ backend/databaseAPI/databaseAPI/databaseAPI/src/test/java/xyz/tamutheo/databaseAPI/MiscellaneousTests.java
@@ -18,12 +18,15 @@ import org.springframework.test.web.servlet.MockMvc;
 import xyz.tamutheo.databaseAPI.appointmentSize.AppointmentSizeController;
 import xyz.tamutheo.databaseAPI.appointmentSize.AppointmentSizeService;
 import xyz.tamutheo.databaseAPI.appointmentSize.AppointmentSizeModel;
+import xyz.tamutheo.databaseAPI.course.CourseController;
+import xyz.tamutheo.databaseAPI.course.CourseService;
+import xyz.tamutheo.databaseAPI.course.CourseModel;
 
 import java.util.Arrays;
 import java.util.List;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(AppointmentSizeController.class)
+@WebMvcTest({AppointmentSizeController.class, CourseController.class})
 public class MiscellaneousTests {
 
     @Autowired
@@ -31,6 +34,9 @@ public class MiscellaneousTests {
 
     @MockBean
     private AppointmentSizeService appointmentSizeService;
+
+    @MockBean
+    private CourseService courseService;
 
     // Test reading appointment sizes with no filters
     @Test
@@ -69,5 +75,70 @@ public class MiscellaneousTests {
                 .andExpect(status().isOk());
 
         verify(appointmentSizeService).read(null, 10, 20);
+    }
+
+    // Test reading courses with no filters
+    @Test
+    public void testReadCoursesNoFilters() throws Exception {
+        when(courseService.read(null, null, null, null, null, null, null)).thenReturn(Arrays.asList(new CourseModel()));
+
+        mockMvc.perform(get("/course")
+                .contentType("application/json"))
+                .andExpect(status().isOk());
+
+        verify(courseService).read(null, null, null, null, null, null, null);
+    }
+
+    // Test reading courses with course number filters
+    @Test
+    public void testReadCoursesWithCourseNumberFilters() throws Exception {
+        when(courseService.read(101, null, null, null, null, null, null)).thenReturn(Arrays.asList(new CourseModel()));
+
+        mockMvc.perform(get("/course")
+                .param("course_number_equals", "101")
+                .contentType("application/json"))
+                .andExpect(status().isOk());
+
+        verify(courseService).read(101, null, null, null, null, null, null);
+    }
+
+    // Test reading courses with course title contains filter
+    @Test
+    public void testReadCoursesWithTitleFilter() throws Exception {
+        when(courseService.read(null, null, null, "Introduction", null, null, null)).thenReturn(Arrays.asList(new CourseModel()));
+
+        mockMvc.perform(get("/course")
+                .param("course_title_contains", "Introduction")
+                .contentType("application/json"))
+                .andExpect(status().isOk());
+
+        verify(courseService).read(null, null, null, "Introduction", null, null, null);
+    }
+
+    // Test reading courses with major abbreviation contains filter
+    @Test
+    public void testReadCoursesWithMajorAbbreviationFilter() throws Exception {
+        when(courseService.read(null, null, null, null, "CS", null, null)).thenReturn(Arrays.asList(new CourseModel()));
+
+        mockMvc.perform(get("/course")
+                .param("major_abbreviation_contains", "CS")
+                .contentType("application/json"))
+                .andExpect(status().isOk());
+
+        verify(courseService).read(null, null, null, null, "CS", null, null);
+    }
+
+    // Test reading courses with pagination
+    @Test
+    public void testReadCoursesWithPagination() throws Exception {
+        when(courseService.read(null, null, null, null, null, 10, 20)).thenReturn(Arrays.asList(new CourseModel()));
+
+        mockMvc.perform(get("/course")
+                .param("limit", "10")
+                .param("offset", "20")
+                .contentType("application/json"))
+                .andExpect(status().isOk());
+
+        verify(courseService).read(null, null, null, null, null, 10, 20);
     }
 }
