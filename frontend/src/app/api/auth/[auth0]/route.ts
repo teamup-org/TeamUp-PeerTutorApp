ad29a89b41877b6de4ba0672206212c869485b33
fornerobj <fornerobj@gmail.com>
2024-06-11 10:30:46 -0500

    Login redirects to dashboard

--- frontend/src/app/api/auth/[auth0]/route.ts
+++ frontend/src/app/api/auth/[auth0]/route.ts
@@ -2,6 +2,7 @@ import { handleAuth, handleLogin } from '@auth0/nextjs-auth0';
 
 export const GET = handleAuth({
     login: handleLogin({
+        returnTo: "/dashboard",
     }),
     signup: handleLogin({
         authorizationParams: {
