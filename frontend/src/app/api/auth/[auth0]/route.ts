635c411cc310e92f5b6cadff0e9e1e517a39383e
Brandon Fornero <fornerobj@gmail.com>
2024-07-01 18:55:44 -0500

    Fixed problem ONLY FOR LOCAL TESTING

--- frontend/src/app/api/auth/[auth0]/route.ts
+++ frontend/src/app/api/auth/[auth0]/route.ts
@@ -1,5 +1,6 @@
-import { handleAuth, handleLogin, handleLogout } from '@auth0/nextjs-auth0';
+import { handleAuth, handleLogin, handleLogout, handleCallback } from '@auth0/nextjs-auth0';
 import { NextApiRequest, NextApiResponse } from 'next';
+import { NextResponse } from 'next/server';
 
 const dynamicHandleLogin = (req: NextApiRequest, res: NextApiResponse) => {
     const url = new URL(req.url, `http://${req.headers.host}`);
@@ -7,6 +8,21 @@ const dynamicHandleLogin = (req: NextApiRequest, res: NextApiResponse) => {
     return handleLogin({
         returnTo,
     })(req, res);
+};
+
+const dynamicHandleCallback = async (req: NextApiRequest, res: NextApiResponse) => {
+    try {
+        await handleCallback(req, res);
+    } catch (error) {
+        console.log(req.headers.host)
+        const errorMessage = (error instanceof Error) ? error.message : 'Unknown error';
+        const redirectUrl = new URL(`/?error=${encodeURIComponent(errorMessage)}`, `http://localhost:3000`);
+        // const redirectUrl = new URL('/', `http://${req.headers.host}`); 
+
+        // Redirect using NextResponse
+        const response = NextResponse.redirect(redirectUrl);
+        return response;
+    }
 };
 
 export const GET = handleAuth({
@@ -20,4 +36,5 @@ export const GET = handleAuth({
     logout: handleLogout({
         returnTo: "/",
     }),
+    callback: dynamicHandleCallback,
 });
