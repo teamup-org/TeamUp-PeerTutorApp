dac1a07f402e46bdc05bbc6d99a094b76d02ef40
fornerobj <fornerobj@gmail.com>
2024-07-23 18:38:37 -0500

    List of appointments

--- frontend/src/app/(ui)/dashboard/recommendation-feed/page.tsx
+++ frontend/src/app/(ui)/dashboard/recommendation-feed/page.tsx
@@ -1,9 +1,35 @@
+'use client';
+import * as React from 'react';
+import { useUser } from '@auth0/nextjs-auth0/client';
+import { TableFetch } from '@/app/_lib/data';
+import { Box, Container, Paper, Stack, Typography } from '@mui/material';
 
-export default function recommnedationPage() {
+export default function RecommendationPage() {
+  const { user } = useUser();
+  const { data: tuteeAppointments, error, refetch } = TableFetch("appointment", [user, "tutee"], `tutee_email_contains=${user?.email}`);
+
+  if (error) return <div>Error loading appointments</div>;
+  if (!tuteeAppointments) return <div>Loading...</div>;
 
   return (
-    <div>
-      <h1>Recommendation Page</h1>
-    </div>
-  )
+    <Box position="static" p={4}>
+      <Container maxWidth="lg" sx={{ minWidth: 750 }}>
+        <Paper variant="outlined" sx={{ px: 4, pb: 4 }}>
+          <Stack direction="column" spacing={4}>
+            <Typography variant="h4">Appointments</Typography>
+            <ul>
+              {tuteeAppointments.data.map((appointment) => (
+                <li key={appointment.id}>
+                  <Typography variant="body1">Tutee: {appointment.tuteeEmail}</Typography>
+                  <Typography variant="body1">Tutor: {appointment.tutorEmail}</Typography>
+                  <Typography variant="body1">Time: {appointment.startDateTimeString}</Typography>
+                  <Typography variant="body1">Subject: {appointment.tuteeRequestComment}</Typography>
+                </li>
+              ))}
+            </ul>
+          </Stack>
+        </Paper>
+      </Container>
+    </Box>
+  );
 }
